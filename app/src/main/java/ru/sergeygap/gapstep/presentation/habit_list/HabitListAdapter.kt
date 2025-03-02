a16d7540d8d02b41237171182de645636dc3a816
sergeygap <1ggserongg1@gmail.com>
2025-03-02 17:12:18 +0300

    clean up code

--- app/src/main/java/ru/sergeygap/gapstep/presentation/habit_list/HabitListAdapter.kt
+++ app/src/main/java/ru/sergeygap/gapstep/presentation/habit_list/HabitListAdapter.kt
@@ -1,7 +1,4 @@
 package ru.sergeygap.gapstep.presentation.habit_list
 
-import androidx.recyclerview.widget.ListAdapter
-import ru.sergeygap.gapstep.domain.Habit
-
-class HabitListAdapter: ListAdapter<Habit, > {
+class HabitListAdapter {
 }
