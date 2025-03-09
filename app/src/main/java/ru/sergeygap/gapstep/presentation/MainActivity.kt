c37e3753e42b706530bda6613e3893012e1051d3
sergeygap <1ggserongg1@gmail.com>
2025-03-09 21:30:23 +0300

    fix edge to edge mapping

--- app/src/main/java/ru/sergeygap/gapstep/presentation/MainActivity.kt
+++ app/src/main/java/ru/sergeygap/gapstep/presentation/MainActivity.kt
@@ -1,7 +1,10 @@
 package ru.sergeygap.gapstep.presentation
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import ru.sergeygap.gapstep.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,15 @@ class MainActivity : AppCompatActivity() {
         // TODO SplashScreenApi
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        edgeToEdgeSetup()
+    }
+
+    private fun edgeToEdgeSetup() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container_view)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
